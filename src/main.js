3f71ef7b98a055bcd61ca3f65b32f3b3a9fbd80f
yli530 <yli530@ucsc.edu>
2021-04-05 17:09:17 -0700

    Setting up Scenes

--- src/main.js
+++ src/main.js
@@ -1,29 +1,12 @@
-function preload() {
-    //this.load.image('ship', '../assets/ship.png');
-}
-
-function create() {
-
-}
-
-function update() {
-
-}
-
-console.log('Hello World! testing live reload');
 let config = {
     type: Phaser.CANVAS,
     width: 640,
     height: 480,
     scale: {
-        mode: Phaser.Scale.FIT,
+        //mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH
     },
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
+    scene: [Menu, Play] 
 };
 
 let game = new Phaser.Game(config);
